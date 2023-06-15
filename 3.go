cc66161b29c6f3f5a8d19978bdebf73585c7324f
eduardoegyto <126253048+eduardoegyto@users.noreply.github.com>
2023-06-15 19:17:58 -0300

    3.go

---
+++ 3.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var x, y, z string
+	fmt.Print("Digite uma palavra: ")
+	fmt.Scanln(&x)
+	fmt.Print("Digite a letra a ser substituído: ")
+	fmt.Scanln(&y)
+	fmt.Print("Digite a nova letra: ")
+	fmt.Scanln(&z)
+
+	nova := strings.ReplaceAll(x, y, z)
+	fmt.Println("A nova palavra é:", nova)
+}
